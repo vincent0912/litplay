425a7584df02a350a939ed2300c099f2a73020ac
Tom Wu <tomyjwu@gmail.com>
2019-03-10 17:51:35 +0800

    hello lit-element

---
+++ src/my-element.ts
@@ -0,0 +1,11 @@
+import { LitElement, html, customElement, property } from 'lit-element';
+
+@customElement('my-element')
+export class MyElement extends LitElement {
+    @property()
+    foo = 'foo';
+
+    render() {
+        return html`<p>${this.foo}</p>`;
+    }
+}
